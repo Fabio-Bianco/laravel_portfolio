f2db0a6a6fe5ac393e233bf79c195359edf1124a
Fabio-Bianco <biancobfabio@gmail.com>
2025-09-18 17:03:14 +0200

    Aggiungi il modello Project e la migrazione per la tabella projects, insieme al seeder per generare dati di esempio

---
+++ database/seeders/ProjectTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+
+class ProjectTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        // Project::truncate(); // Rimuovi o commenta questa riga per non cancellare i record esistenti
+
+        for($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(3);
+            $newProject->description = $faker->paragraph();
+            $newProject->image = 'placeholder.png';
+            $newProject->link = $faker->url();
+            $newProject->save();
+        }
+    }
+}
+
